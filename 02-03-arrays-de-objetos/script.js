f0ae90827faba6fcf3b7e708816743cffc30fd8e
Rafael Madakis <rafaelmadakis@bol.com.br>
2021-07-23 23:43:53 -0300

    02-03-arrays-de-objetos

---
+++ 02-03-arrays-de-objetos/script.js
@@ -0,0 +1,23 @@
+var person1 = {
+  name: 'Daniel',
+  age: 22
+}
+
+var person2 = {
+  name:'Rafaela',
+  age: 32
+}
+
+var person3 = {
+  name: 'Caio',
+  age: 36
+}
+
+var list = [person1, person2, person3]
+
+//iterar sobre list
+for (var person of list) {
+  console.log(person.name)
+}
+
+
