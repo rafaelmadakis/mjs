e3fb9a62adcab2d56c661126a5cac8b4a7ff7230
Rafael Madakis <rafaelmadakis@bol.com.br>
2021-07-25 19:43:18 -0300

    03-3-entendendo-o-uso-do-var

---
+++ 03-03-entendendo-o-uso-do-var/index.js
@@ -0,0 +1,13 @@
+// function showAge() {
+//   var age = 21
+// }
+
+var age  = 21
+ age = 22
+
+// if(age > 18) {
+//   var test = 35
+// }
+
+
+console.log(age)Hi D G, There is some use for this feature. You can update your key and most dealers will do a key read at no charge. When fault codes are present some codes link to specific SIBs or service information bulletins which can sometimes identify bulletins relating to some odd symptoms. There is no charge for a key read. It can also identify if there are any service actions or open campaigns on your vehicle. Recalls and service actions are always free of charge. I thought this would be a good bit of information to pass on. Thanks for posting!
