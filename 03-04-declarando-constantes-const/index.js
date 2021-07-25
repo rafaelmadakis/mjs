55651d2a1a4b8802ab83e4c57cd4d45883625a1c
Rafael Madakis <rafaelmadakis@bol.com.br>
2021-07-25 19:59:45 -0300

    03-04-declarando-constantes-com-const

---
+++ 03-04-declarando-constantes-const/index.js
@@ -0,0 +1,15 @@
+// const me = {
+//   name: 'Daniel',
+//   age = 21
+// }
+
+// const list = ['banana', 'batata']
+// list.push('morango')
+
+const age =21
+if(age > 18){
+  const newAge = 35
+  const passYears = age - 18
+  console.log(passYears, newAge)
+}
+
