fb8f3d1a2e929d277d0a178a9dc4250b7d717696
Rafael Madakis <rafaelmadakis@bol.com.br>
2021-07-27 22:43:11 -0300

    03-11-trabalhando-com-template-strings

---
+++ 03-11-trabalhando-com-template-strings/index.js
@@ -0,0 +1,10 @@
+const name = 'Julio'
+const age = 32
+
+const sentence = 'olá, meu nome é  ' + name + ' e eu tenho ' + age + ' anos'
+
+const sentence2 = `olá, meu nome é ${name} e eu 
+tenho ${age} anos` // template string
+
+console.log(sentence)
+console.log(sentence2)
