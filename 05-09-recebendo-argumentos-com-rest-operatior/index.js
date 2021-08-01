3bcb8415955b199453afe3844c5d969c05e81060
Rafael Madakis <rafaelmadakis@bol.com.br>
2021-07-31 23:01:26 -0300

    05-09-recebendo-argumentos-com-rest-operatior

---
+++ 05-09-recebendo-argumentos-com-rest-operatior/index.js
@@ -0,0 +1,9 @@
+function createUser(name, age, ...contacts) {
+  return {
+    name,
+    age,
+    contacts
+  }
+}
+
+console.log(createUser('Lucas', 32, 'email@email1.com', 'email@email2.com', 'email@email3.com', 'email@email4.com', 'email@email5.com'))
