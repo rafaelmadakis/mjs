134c91e8b20b850e184e6981811bc159ae6181cc
Rafael Madakis <rafaelmadakis@bol.com.br>
2021-07-31 22:37:11 -0300

    05-06-clonando-objetos-com-spread

---
+++ 05-06-clonando-objetos-com-spread/index.js
@@ -0,0 +1,12 @@
+const user = {
+  name: 'Lucas',
+  email: 'lucas@email.com'
+}
+
+const newUser = {...user} //operador spread / clone
+
+// newUser.email = 'joao@zinho.com'
+// newUser.name = 'joaozinho123'
+
+
+console.log(newUser)
