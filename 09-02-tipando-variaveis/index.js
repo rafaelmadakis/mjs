91a492a664ca6865f3bb679d25e855046d0a9c58
Rafael Madakis <rafaelmadakis@bol.com.br>
2021-08-03 22:25:33 -0300

    09-02-tipando-variaveis

---
+++ 09-02-tipando-variaveis/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var personName = 'Daniel';
+var personAge = 22;
+var personAlive = true;
+var personContacts = [
+    'daniel@email.com',
+    'daniel2@email.com',
+    0
+];
---
+++ 09-02-tipando-variaveis/index.ts
@@ -0,0 +1,8 @@
+const personName:string = 'Daniel'
+const personAge:number = 22
+const personAlive:boolean = true
+const personContacts:(string| number)[] = [ // aceita aarrys de string ou de números]
+  'daniel@email.com',
+  'daniel2@email.com',
+  0
+]
