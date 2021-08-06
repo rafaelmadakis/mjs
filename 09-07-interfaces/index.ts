4fa9d86842435be5a5bdaa7fe0764ff0a6f19953
Rafael Madakis <rafaelmadakis@bol.com.br>
2021-08-05 22:50:55 -0300

    09-07-interfaces

---
+++ 09-07-interfaces/index.ts
@@ -0,0 +1,17 @@
+interface Animal {
+  sex: 'male' | 'female'
+}
+
+interface Human extends Animal  {
+  name: string
+  age: number  
+}
+
+type Person = Human & Animal
+
+
+const person: Human = {
+  age: 22,
+  name: 'Lucas',
+  sex: 'male'
+}
